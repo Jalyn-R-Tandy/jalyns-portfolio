2a6537061c3a97ca2b8a02af44b1720d42350618
Jalyn Tandy <jalynrtandy@gmail.com>
2025-12-04 03:26:59 -0600

    Added tab icon

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -5,10 +5,10 @@ const Projects = () => {
   return (
     <section id='projects' className="max-w-full h-max bg-darker-navy border-white/5 border rounded-4xl p-8 relative -top-12">
         <h2 className="font-semibold text-[32px] mb-8 text-brand-text-primary text-center lg:text-start lg:ml-16">My Projects</h2>
-        <div className='flex flex-col justify-center items-center gap-16 lg:flex-row lg:gap-8 xl:gap-24'>
+        <div className='flex flex-col justify-center items-center p-16 gap-16 lg:flex-row lg:gap-8 xl:gap-24'>
             <Card category="Full Stack" project="ResuLink" desc="This is the description, app coming soon." stack="React" img="/resulink-img.png"/>
             <Card category="Mobile Development" project="GradPathAI" desc="This is the description, app coming soon." stack="Next.js"/>
-            <Card />
+            <Card category="Web Development" project="Spotify Extension" desc="This is the description, app coming soon." stack="Next.js" />
         </div>
     </section>
   )
