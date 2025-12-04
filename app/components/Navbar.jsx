b2dc6a813d20ea4cb7912197a1c67573a49b39f5
Jalyn Tandy <jalynrtandy@gmail.com>
2025-12-04 15:53:00 -0600

    Fixing some style issues

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,8 +2,7 @@
 /** TODO: Add a function so that when hovering over nav links other than home, it transfers the white bubble around it to the link hovered on currently */
 const Navbar = () => {
   return (
-    <nav className='w-full flex justify-center absolute top-0 right-0 left-0 z-20'>
-        <div className='h-16 lg:h-18 min-w-11/12 flex justify-center items-center rounded-full mt-8 bg-deep-navy/50 backdrop-blur-3xl border border-border-gray/50'>
+    <nav className='w-full flex justify-center absolute top-0 right-0 left-0 z-20'><div className='h-16 lg:h-18 min-w-11/12 flex justify-center items-center rounded-full mt-8 bg-deep-navy/50 backdrop-blur-3xl border border-border-gray/50'>
             <ul className=' flex justify-around items-center max-w-4xl'>
                 <a href="#home">
                     <li className='font-medium px-8 sm:px-12 lg:px-16 py-3 lg:py-4 mr-2 text-light-gray hover:text-hot-red rounded-full ease-in-out duration-200'>Home</li>
